72b5ccfd47e88010ec906273fe4f56a1591dd418
YadanarMyoThet13199 <yadanarmyothet1999@gmail.com>
2018-08-02 03:51:31 +0630

    sign in and sign up are not included

--- app/src/main/java/com/example/lenovo/edulight/NewfeedFragment.java
+++ app/src/main/java/com/example/lenovo/edulight/NewfeedFragment.java
@@ -1,5 +1,6 @@
 package com.example.lenovo.edulight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -8,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class NewfeedFragment extends Fragment {
-    private FloatingActionButton user_post_upload_btn;
+    private FloatingActionButton new_feed_upload_post_btn;
     public NewfeedFragment()
     {
 
@@ -16,6 +17,14 @@ public class NewfeedFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_feed,container,false);
+        new_feed_upload_post_btn = (FloatingActionButton)view.findViewById(R.id.new_feed_upload_post_btn);
+        new_feed_upload_post_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),PostUploadActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
